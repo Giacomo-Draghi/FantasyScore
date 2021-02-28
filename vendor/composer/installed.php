ddb463da6b2428cea46440ea9da935ed42ff0144
giacomo draghi <draghi96@libero.it>
2021-02-28 21:04:12 +0100

    inizializing

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '887a1855de983a1f1ba1ec941e4fc4b5bbd5170b',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '887a1855de983a1f1ba1ec941e4fc4b5bbd5170b',
+    ),
+  ),
+);
